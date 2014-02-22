5f4be3847901c600105ab9a74a7dc2c5629d6eb5
Rick Murt <rickmurt@gmail.com>
2014-02-22 17:00:16 -0500

    Players controller: adding it to app and public facing app js

---
+++ public/js/controllers/players.js
@@ -0,0 +1,10 @@
+window.angular.module('ngff.controllers.players', [])
+	.controller('PlayersController', ['$scope', 'Global', 'NFL',
+		function ($scope, Global, NFL) {
+			$scope.global = Global;
+
+			$scope.positions = NFL.positions;
+			$scope.nflteams = NFL.teams;
+			$scope.limitct = 10;
+		}
+	]);
